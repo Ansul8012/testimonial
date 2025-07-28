f6eaacd19aae9aaa1eb6485a78edfa2089922a2c
Ansul <ansulsingh171@gmail.com>
2025-07-28 15:47:06 +0530

    done testimonial component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TestimonialSection from "./component/TestimonialSection";
+
+const App = () => {
+  return (
+    <div>
+      <TestimonialSection />
+    </div>
+  );
+};
+
+export default App;
